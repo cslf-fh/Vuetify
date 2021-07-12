9ce652e9ba6401cd93fc3e5c28fcdd3fa0d645e6
cslf-fh <consadole.sapporo.love.football@gmail.com>
2021-07-13 02:59:40 +0900

    add LinkText.vue

---
+++ src/components/LinkText.vue
@@ -0,0 +1,42 @@
+<template>
+  <span>
+    <v-btn
+      class="px-0"
+      color="primary"
+      height="24"
+      text
+      :to="to"
+      :href="href"
+      :target="target"
+    >
+      <template v-slot>
+        <span class="text-subtitle-1">{{ text }}</span>
+        <v-icon color="primary" small>{{ icon }}</v-icon>
+      </template>
+    </v-btn>
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'LinkText',
+  props: {
+    link: String,
+    url: String,
+    text: String,
+    icon: String,
+  },
+  data() {
+    return {
+      to: false,
+      href: false,
+      target: '',
+    };
+  },
+  created() {
+    this.$props.link === 'to'
+      ? (this.to = this.$props.url)
+      : ((this.href = this.$props.url), (this.target = '_blank'));
+  },
+};
+</script>
